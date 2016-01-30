a5d8327bf00b89f7cb56a07af7beb37b9d62aa84
yifanxing <xing.yif@Husky.neu.edu>
2016-01-30 12:50:44 -0500

    Added Comments

--- app/src/main/java/com/example/yifanxing/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yifanxing/myapplication/MainActivity.java
@@ -60,6 +60,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+// main functions of the application
 public class MainActivity extends Activity {
     private static final int OPEN_PHOTO_FOLDER_REQUEST_CODE = 1;
     double calorieCount = 0.0;
@@ -71,6 +72,8 @@ public class MainActivity extends Activity {
 
     Logger mLogger = Logger.getLogger("APP");
 
+    // setup the main screen and buttons
+    // and send the correct information to the right dialog
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,6 +94,7 @@ public class MainActivity extends Activity {
                 startActivityForResult(camera_intent, CAM_REQUEST);
             }
         });
+        // resets the calories to 0
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,12 +105,13 @@ public class MainActivity extends Activity {
 
     }
 
-
+    // get the photo took by the user
     private File getFile() {
         File internalDir = getExternalCacheDir();
         return new File(internalDir, "cam_image.jpg");
     }
 
+    // convert the photo that the user took from jpg to base64
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         mLogger.warning("==== Received file: ");
@@ -122,6 +127,7 @@ public class MainActivity extends Activity {
         new ImageIdentifier().execute(ba1);
     }
 
+    // dialog of food items shown on the main screen
     public void alertSimpleListView(List<String> items) {
         //list of food items to be shown in alert dialog
         final String[] array = items.toArray(new String[items.size()]);
@@ -139,8 +145,10 @@ public class MainActivity extends Activity {
         }).show();
     }
 
+    // link to the food identifier(MetaMind) API and send image to the API
     public class ImageIdentifier extends AsyncTask<String, Void, JSONObject> {
-        private static final String apiKey = "i8w9VkxHx45Sb6JuYUDZWg4CNsOfveZRvz8RuMrjQXMrPcMeTb";
+        private static final String apiKey =
+                "i8w9VkxHx45Sb6JuYUDZWg4CNsOfveZRvz8RuMrjQXMrPcMeTb";
         @Override
         protected JSONObject doInBackground (String...params){
             String image = params[0];
@@ -182,6 +190,7 @@ public class MainActivity extends Activity {
             return error;
         }
 
+        // posts the predictions of the food(from API) to dialog
         @Override
         protected void onPostExecute(JSONObject o){
             //Log.d("objects", o.toString());
@@ -190,14 +199,16 @@ public class MainActivity extends Activity {
             for (int i = 0; i < predictions.length(); i++) {
                 classNames.add(predictions.optJSONObject(i).optString("class_name"));
             }
-            alertSimpleListView(classNames);
+            alertSimpleListView(classNames); // sends the list of foods to the dialog
         }
     }
 
+    // link to the calories library, nutritionix API
     public class getCalories extends AsyncTask<String, Void, String> {
         final String appId = "69f1b02f";
         final String apiKey = "27ddbb03562b1460fe0d834e0a0c699f";
 
+        // sends food name and get the calories and fat
         @Override
         protected String doInBackground (String...params) {
             String foodName = params[0];
@@ -220,9 +231,9 @@ public class MainActivity extends Activity {
                 JSONArray hits = jsnobject.optJSONArray("hits");
                 String hitsString = hits.getString(0);
                 JSONObject fields = new JSONObject(hitsString).getJSONObject("fields");
-                String calories = fields.getString("nf_calories");
-                String fat = fields.getString("nf_total_fat");
-                showCalorieDataDialog(foodName, calories, fat);
+                String calories = fields.getString("nf_calories"); // gets the calories from API
+                String fat = fields.getString("nf_total_fat"); // gets the fat from API
+                showCalorieDataDialog(foodName, calories, fat); // sends the info to dialog
                 //Log.d("calorieData", fat);
                 return responseData;
             } catch (ClientProtocolException e) {
@@ -239,6 +250,7 @@ public class MainActivity extends Activity {
         }
     }
 
+    // draws the dialog that shows the calories of the food
     public void showCalorieDataDialog(final String foodName, final String calories, final String fat){
         final DecimalFormat df2 = new DecimalFormat(".##");
         new Thread()
@@ -250,7 +262,10 @@ public class MainActivity extends Activity {
                     public void run()
                     {
                         new AlertDialog.Builder(MainActivity.this)
+                                // asks the user whether he or she still wants to eat the food
+                                // after seeing the calories and fat the food contains
                                 .setTitle("Eat " + foodName + "?")
+                                // prints the calories and fat
                                 .setMessage("Calories: " + calories + "\n" + "Fat: " + fat + "grams")
                                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int which) {
@@ -258,12 +273,13 @@ public class MainActivity extends Activity {
                                         calorieCount = calorieCount + Double.parseDouble(calories);
                                         System.out.println(calorieCount);
                                         //double calorieCount= %.2e;
-                                        //textView2.setText(String.format( "%.2f", calorieCount ));
+                                        // add "Total Calories" after the calories number
                                         textView2.setText(df2.format(calorieCount) + " Total Calories");
                                         dialog.dismiss();
                                     }
                                 })
-                                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                .setNegativeButton(android.R.string.no,
+                                        new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int which) {
                                         //do nothing
                                     }
